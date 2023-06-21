a53f1726465721f5a857e285018569965b673b4e
Ricardo Primi <ricardoprimi@Mac-mini-de-Ricardo.local>
2023-06-21 09:45:29 -0300

    Dados python -> R

---
+++ python/db_config.py
@@ -0,0 +1,23 @@
+import os
+os.environ['LABAPE6_HOST'] = 'mysql08.labape.com.br'
+os.environ['LABAPE6_USER'] = 'labape6'
+os.environ['LABAPE6_PASSWORD'] = 'yyzrushk98159'
+os.environ['LABAPE6_DBNAME'] = 'labape6'
+
+
+# Establish the connection
+conn = pymysql.connect(
+    host = os.getenv('LABAPE6_HOST'),
+    user = os.getenv('LABAPE6_USER'),
+    password = os.getenv('LABAPE6_PASSWORD'),
+    db = os.getenv('LABAPE6_DBNAME')
+)
+
+
+db_config = {
+    'user': 'labape6',
+    'password': 'mypassword',
+    'host':  'mysql08.labape.com.br',
+    'database': 'labape6',
+    'raise_on_warnings': True
+}
